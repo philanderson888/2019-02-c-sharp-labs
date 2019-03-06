a3b02c35a0a6d9b6f8a3f84d5ad0d5202c144a23
philandersonsparta <panderson@spartaglobal.com>
2019-03-06 16:24:57 +0000

    Labs 31-34 Struct, Entity Code First, Events, Website Core

---
+++ labs/lab_31_struct/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace lab_31_struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var P01 = new Point(10, 10);
+            var P02 = new Point(20, 20);
+        }
+    }
+
+    class X { }
+
+    struct Point
+    {
+        public int X;
+        public int Y;
+        // CONSTRUCTOR
+        public Point(int X, int Y)
+        {
+            this.X = X;
+            this.Y = Y;
+        }
+    }
+
+
+
+
+}
